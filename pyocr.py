d32e460d9e3572e841658adeaa4944ec62d8c2a6
mohan3d <mohan3d94@gmail.com>
2016-01-10 17:03:29 +0200

    Fixed typo, minor changes to work with both python2 and python3

--- pyocr.py
+++ pyocr.py
@@ -2,7 +2,7 @@ from PIL import Image
 import sys
 
 
-def ocr(im, threshold=200, mask="letters.bmp", aplhabet="0123456789abcdef"):
+def ocr(im, threshold=200, mask="letters.bmp", alphabet="0123456789abcdef"):
     img = Image.open(im)
     img = img.convert("RGB")
     box = (8, 8, 58, 18)
@@ -19,19 +19,19 @@ def ocr(im, threshold=200, mask="letters.bmp", aplhabet="0123456789abcdef"):
         mx = 1000000
         max_x = 0
         x = 0
-        for x in xrange(img.size[0] - letter.size[0]):
-            sum = 0
-            for i in xrange(letter.size[0]):
-                for j in xrange(letter.size[1]):
-                    sum = sum + abs(A[x + i, j][0] - B[i, j][0])
-            if sum < mx:
-                mx = sum
+        for x in range(img.size[0] - letter.size[0]):
+            _sum = 0
+            for i in range(letter.size[0]):
+                for j in range(letter.size[1]):
+                    _sum = _sum + abs(A[x + i, j][0] - B[i, j][0])
+            if _sum < mx:
+                mx = _sum
                 max_x = x
-        return (mx, max_x)
+        return mx, max_x
 
     # Clean the background noise, if color != black, then set to white.
-    for y in xrange(img.size[1]):
-        for x in xrange(img.size[0]):
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
             if not (pixdata[x, y][0] > threshold) and \
                     (pixdata[x, y][1] > threshold) and \
                     (pixdata[x, y][2] > threshold):
@@ -45,9 +45,9 @@ def ocr(im, threshold=200, mask="letters.bmp", aplhabet="0123456789abcdef"):
 
     letterlist = []
 
-    for x in xrange(letters.size[0]):
+    for x in range(letters.size[0]):
         black = True
-        for y in xrange(letters.size[1]):
+        for y in range(letters.size[1]):
             if ledata[x, y][0] != 0:
                 black = False
                 break
@@ -56,19 +56,19 @@ def ocr(im, threshold=200, mask="letters.bmp", aplhabet="0123456789abcdef"):
                 box = (old_x + 1, 0, x, 10)
                 letter = letters.crop(box)
                 t = test_letter(img, letter)
-                letterlist.append((t[0], aplhabet[counter], t[1]))
+                letterlist.append((t[0], alphabet[counter], t[1]))
             old_x = x
-            counter = counter + 1
+            counter += 1
 
     box = (old_x + 1, 0, 140, 10)
     letter = letters.crop(box)
     t = test_letter(img, letter)
-    letterlist.append((t[0], aplhabet[counter], t[1]))
+    letterlist.append((t[0], alphabet[counter], t[1]))
 
     t = sorted(letterlist)
     t = t[0:5]  # 5-letter captcha
 
-    final = sorted(t, key=lambda x: x[2])
+    final = sorted(t, key=lambda e: e[2])
     answer = ""
     for l in final:
         answer = answer + l[1]
@@ -76,5 +76,5 @@ def ocr(im, threshold=200, mask="letters.bmp", aplhabet="0123456789abcdef"):
 
 
 if __name__ == '__main__':
-    print ocr(sys.argv[1])
+    print(ocr(sys.argv[1]))
 
